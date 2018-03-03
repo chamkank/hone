dc8067b2d18eba1fe38696a879aeb680a5f57576
CK <chamkank@users.noreply.github.com>
2018-03-03 12:41:56 -0800

    add support for commas in cells

--- csv_utils.py
+++ csv_utils.py
@@ -16,18 +16,39 @@ class CSVUtils:
     def get_data_rows(self):
         data_rows = []
         f = open(self.filepath, 'r')
-        row = f.readline()
+        f.readline()  # discard column names
         while True:
             row = f.readline()
             if row != '':
-                data_rows += self.parse_row(f.readline())
+                data_rows.append(self.parse_row(row))
             else:
                 break
         f.close()
-        return data_rows[:-1]
+        return data_rows
 
     @staticmethod
     def parse_row(row):
-        row = row[:-1].split(',')
-        cells = [cell.strip(' ') for cell in row]
+        row = row[:-1]
+        cells = []
+
+        quote_flag = False
+        split_index = 0
+        i = 0
+
+        # parse rows
+        while i < len(row):
+            c = row[i]
+            if c == '"':
+                quote_flag = not quote_flag
+            elif c == ',':
+                if not quote_flag:
+                    cells.append(row[split_index:i])
+                    split_index = i+1
+            i += 1
+
+        # add last cell in row
+        if split_index != len(row)-1:
+            cells.append(row[split_index:])
+
+        cells = [cell.strip(' "') for cell in cells]
         return cells
