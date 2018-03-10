0916fe7b0c2829519bce46eb82ed76b58018e658
CK <chamkank@users.noreply.github.com>
2018-03-09 23:51:16 -0800

    add helper method stubs & docstrings

--- tablenest.py
+++ tablenest.py
@@ -1,20 +1,58 @@
+import json
 from utils import csv_utils
 
 
 class TableNest:
-    def __init__(self):
-        self.delimit_chars = [' ', '_', ',']
 
-    def tablenest(self, column_names):
+    def __init__(self, csv_filepath):
+        self.delimit_chars = [' ', '_', ',']
+        self.csv_filepath = csv_filepath
+        self.csv = csv_utils.CSVUtils(self.csv_filepath)
+
+    '''
+    Returns nested JSON generated from given CSV file as string.
+    '''
+
+    def get_json_string(self):
+        column_names = self.csv.get_column_names()
+        data_rows = self.csv.get_data_rows()
+        column_structure = self.generate_full_structure(column_names)
+        nested_json = self.populate_structure_with_data(column_structure, data_rows)
+        return json.dumps(nested_json)
+
+    '''
+    Returns dictionary with given data rows fitted to given structure.
+    '''
+
+    def populate_structure_with_data(self, column_structure, data_rows):
+        return {}  # TODO
+
+    '''
+    Returns dictionary with column names mapped to final JSON path in given structure
+    
+    >> map_structure_to_columns(['birth day', 'birth month'], 
+                                {'birth':{'day':{}, 'month':{}})
+    {'birth day':'birth.day', 'birth month': 'birth.month'}
+    '''
+
+    def map_structure_to_columns(self, column_names, structure):
+        pass  # TODO
+
+    '''
+    Generate recursively-nested JSON structure from column_names.
+    '''
+
+    def generate_full_structure(self, column_names):
         structure = self.get_nested_structure(column_names)
+        print(structure)
         if len(structure) > 1:
             for k, v in structure.items():
                 if v != {}:
-                    structure[k] = self.tablenest(v)
+                    structure[k] = self.generate_full_structure(v)
         return structure
 
     '''
-    Returns optimal structure for nested JSON based on list of column names.
+    Returns structure with maximum depth = 1 based on list of column names.
     '''
 
     def get_nested_structure(self, column_names):
@@ -89,5 +127,5 @@ class TableNest:
 if __name__ == '__main__':
     control = TableNest()
     print(control.get_valid_splits("birth date __date"))
-    print(control.tablenest(['name', 'age (years)', 'weight (kg)', 'birth day',
-                             'birth month', 'birth year', 'adopted', 'adopted_since']))
+    print(control.generate_full_structure(['name', 'age (years)', 'weight (kg)', 'birth day',
+                             'birth month a', 'birth month b', 'birth year', 'adopted', 'adopted_since']))
