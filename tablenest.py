f66cab9fd07c291403d7fde3ee694ea447ebaf99
CK <chamkank@users.noreply.github.com>
2018-04-30 20:11:52 -0700

    Add column-preserving recursive nesting
    Generates a nested JSON structure that now preserves the initial column name at each leaf. What's left to be done is parsing the CSV data into this generate structure.

--- tablenest.py
+++ tablenest.py
@@ -28,69 +28,60 @@ class TableNest:
         return {}  # TODO
 
     '''
-    Returns dictionary with column names mapped to final JSON path in given structure
-    
-    >> map_structure_to_columns(['birth day', 'birth month'], 
-                                {'birth':{'day':{}, 'month':{}})
-    {'birth day':'birth.day', 'birth month': 'birth.month'}
-    '''
-
-    def map_structure_to_columns(self, structure, column_names):
-        mapping = {}
-        for col in column_names:
-            if col in structure.keys():
-                mapping[col] = col
-                continue
-            else:
-                # TODO
-        return mapping
-
-
-
-    '''
     Generate recursively-nested JSON structure from column_names.
     '''
-
     def generate_full_structure(self, column_names):
-        structure = self.get_nested_structure(column_names)
-        if len(structure) > 1:
-            for k, v in structure.items():
-                if v != {}:
-                    structure[k] = self.generate_full_structure(v)
-        return structure
-
-    '''
-    Returns structure with maximum depth = 1 based on list of column names.
-    '''
-
-    def get_nested_structure(self, column_names):
-        already_visited = []
+        visited = set()
         structure = {}
         sorted(column_names)
+        column_names = column_names[::-1]
         for c1 in column_names:
-            if c1 in already_visited:
+            if c1 in visited:
                 continue
             splits = self.get_valid_splits(c1)
             for split in splits:
-                nodes = {split: []}
+                nodes = {split: {}}
+                if split in column_names:
+                    continue
                 for c2 in column_names:
-                    if c2.startswith(split):
-                        if split == c2:
-                            break
-                        else:
-                            nodes[split].append(self.get_split_suffix(split, c2))
-                            if c2 not in already_visited:
-                                already_visited.append(c2)
-                if len(nodes[split]) > 1:
-                    structure[split] = nodes[split]
-                else:
-                    structure[c1] = {}
-            if c1 not in already_visited:  # if column_name not nested
-                structure[c1] = {}
+                    if c2 not in visited and self.is_valid_prefix(split, c2):
+                        nodes[split][self.get_split_suffix(split, c2)] = c2
+                if len(nodes[split].keys()) > 1:
+                    structure[split] = self.get_nested_structure(nodes[split])
+                    for val in nodes[split].values():
+                        visited.add(val)
+            if c1 not in visited:  # if column_name not nestable
+                structure[c1] = c1
         return structure
 
     '''
-    Returns all valid splits for a given column name
+    Generate nested JSON structure given parent structure generated from initial call to get_full_structure
+    '''
+    def get_nested_structure(self, parent_structure):
+        column_names = list(parent_structure.keys())
+        visited = set()
+        structure = {}
+        sorted(column_names, reverse=True)
+        for c1 in column_names:
+            if c1 in visited:
+                continue
+            splits = self.get_valid_splits(c1)
+            for split in splits:
+                nodes = {split: {}}
+                if split in column_names:
+                    continue
+                for c2 in column_names:
+                    if c2 not in visited and self.is_valid_prefix(split, c2):
+                        nodes[split][self.get_split_suffix(split, c2)] = parent_structure[c2]
+                        visited.add(c2)
+                if len(nodes[split].keys()) > 1:
+                    structure[split] = self.get_nested_structure(nodes[split])
+            if c1 not in visited:  # if column_name not nestable
+                structure[c1] = parent_structure[c1]
+        return structure
+
+    '''
+    Returns all valid splits for a given column name in descending order by length
     '''
 
     def get_valid_splits(self, column_name):
@@ -102,7 +93,7 @@ class TableNest:
                 split = self.clean_split(column_name[0:i])
                 splits.append(split)
             i -= 1
-        return list(set(splits))
+        return sorted(list(set(splits)), reverse=True)
 
     '''
     Returns string after split without delimiting characters.
@@ -131,11 +122,18 @@ class TableNest:
             i -= 1
         return split
 
+    '''
+    Returns true if str_a is a valid prefix of str_b
+    '''
+    def is_valid_prefix(self, prefix, base):
+        if base.startswith(prefix):
+            if base[len(prefix)] in self.delimit_chars:
+                return True
+        return False
+
 
 if __name__ == '__main__':
     control = TableNest("/tests/data/small_cats_dataset")
-    print(control.get_valid_splits("birth date __date"))
-    column_names = {'name', 'age (years)', 'weight (kg)', 'birth day',
-                             'birth month a', 'birth month b', 'birth year', 'adopted', 'adopted_since'}
+    column_names = ['e','a','a_b','b c d e', 'b c d f', 'b c e f']
     struct = control.generate_full_structure(column_names)
-    print(control.map_structure_to_columns(struct, column_names))
+    print(struct)
