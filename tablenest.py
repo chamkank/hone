bb700dc45f9597c2b3e83f5333834d0daced4760
CK <chamkank@users.noreply.github.com>
2018-05-01 18:59:10 -0700

    finish automatic nesting function

--- tablenest.py
+++ tablenest.py
@@ -3,7 +3,6 @@ from utils import csv_utils
 
 
 class TableNest:
-
     def __init__(self, csv_filepath):
         self.delimit_chars = [' ', '_', ',']
         self.csv_filepath = csv_filepath
@@ -16,20 +15,41 @@ class TableNest:
     def get_json_string(self):
         column_names = self.csv.get_column_names()
         data_rows = self.csv.get_data_rows()
-        column_structure = self.generate_full_structure(column_names)
-        nested_json = self.populate_structure_with_data(column_structure, data_rows)
+        structure = self.generate_full_structure(column_names)
+        nested_json = self.populate_structure_with_data(structure, column_names, data_rows)
         return json.dumps(nested_json)
 
+    '''
+    Perform automatic nesting
+    '''
+    def autonest(self):
+        column_names = self.csv.get_column_names()
+        data = self.csv.get_data_rows()
+        column_struct = self.generate_full_structure(column_names)
+        json_struct = self.populate_structure_with_data(column_struct, column_names, data)
+        return json_struct
     '''
     Returns dictionary with given data rows fitted to given structure.
     '''
 
-    def populate_structure_with_data(self, column_structure, data_rows):
-        return {}  # TODO
-
+    def populate_structure_with_data(self, structure, column_names, data_rows):
+        json_struct = []
+        num_columns = len(column_names)
+        mapping = self.get_leaves(structure)
+        for row in data_rows:
+            json_row, i = structure, 0
+            while i < num_columns:
+                cell = row[i]
+                column_name = column_names[i]
+                key_path = mapping[column_name]
+                exec("json_row"+key_path+"="+"'"+cell+"'")
+                i += 1
+            json_struct.append(json_row)
+        return json_struct
     '''
     Generate recursively-nested JSON structure from column_names.
     '''
+
     def generate_full_structure(self, column_names):
         visited = set()
         structure = {}
@@ -57,6 +77,7 @@ class TableNest:
     '''
     Generate nested JSON structure given parent structure generated from initial call to get_full_structure
     '''
+
     def get_nested_structure(self, parent_structure):
         column_names = list(parent_structure.keys())
         visited = set()
@@ -79,6 +100,26 @@ class TableNest:
             if c1 not in visited:  # if column_name not nestable
                 structure[c1] = parent_structure[c1]
         return structure
+
+    '''
+    Get the leaf nodes of a nested structure and the path to those nodes.
+    Ex: {"a":{"b":"c"}} => {"c":"['a']['b']"}
+    '''
+
+    def get_leaves(self, structure, path="", result={}):
+        for key, value in structure.items():
+            if type(value) is dict:
+                self.get_leaves(value, path+"['"+key+"']", result)
+            else:
+                result[value] = path+"['"+key+"']"
+        return result
+
+    '''
+    Merge two dictionaries and return the result.
+    '''
+
+    def merge_dictionaries(self, a, b):
+        return {**a, **b}
 
     '''
     Returns all valid splits for a given column name in descending order by length
@@ -125,15 +166,9 @@ class TableNest:
     '''
     Returns true if str_a is a valid prefix of str_b
     '''
+
     def is_valid_prefix(self, prefix, base):
         if base.startswith(prefix):
             if base[len(prefix)] in self.delimit_chars:
                 return True
         return False
-
-
-if __name__ == '__main__':
-    control = TableNest("/tests/data/small_cats_dataset")
-    column_names = ['e','a','a_b','b c d e', 'b c d f', 'b c e f']
-    struct = control.generate_full_structure(column_names)
-    print(struct)
