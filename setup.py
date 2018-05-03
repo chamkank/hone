664b0830b7e16fd42ee2ec91964bc743bc0a7cf6
CK <chamkank@users.noreply.github.com>
2018-05-02 21:03:57 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
+import os
 from setuptools import setup, find_packages
+
+long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 setup(
     name='hone',
-    version='0.1.0',
+    version='0.1.3',
     author='Chamantha Kankanamge',
     author_email='chamkdev@gmail.com',
     license='MIT',
     description='Convert CSV to automatically nested JSON.',
+    long_description=long_description,
     keywords="convert csv auto nested json",
     url='https://github.com/chamkank/hone',
     project_urls={
@@ -15,9 +19,6 @@ setup(
     packages=find_packages(),
     install_requires=[],
     entry_points={'console_scripts': ['hone=hone.__main__:main']},
-    package_data={
-        'hone': ['config.json'],
-    },
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
