b97e91dde34c7c1131f6c70470513449cffb8d51
CK <chamkank@users.noreply.github.com>
2018-05-02 18:59:16 -0700

    update setup.py for distribution

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='Hone',
-    version='0.1',
+    version='0.1.0',
     author='Chamantha Kankanamge',
     author_email='chamkdev@gmail.com',
     license='MIT',
     description='Convert CSV to automatically nested JSON.',
-    keywords="convert csv to nested json",
+    keywords="convert csv auto nested json",
     url='https://github.com/chamkank/hone',
     project_urls={
         "Source Code": 'https://github.com/chamkank/hone'
     },
-    packages=find_packages()
+    packages=['hone'],
+    install_requires=[],
+    entry_points={'setuptools.installation': [
+        'eggsecutable = hone:__main__'
+    ]},
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
 )
 
