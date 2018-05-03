5599a0a6e6799d8836103a03a660d2df7904823f
CK <chamkank@users.noreply.github.com>
2018-05-02 17:22:01 -0700

    add pretty printing

--- hone/utils/json_utils.py
+++ hone/utils/json_utils.py
@@ -10,4 +10,4 @@ Write given JSON to given file.
 
 def save_json(json_struct, json_filepath):
     with open(json_filepath, 'w') as f:
-        json.dump(json_struct, f)
+        json.dump(json_struct, f, indent=2, sort_keys=True)
