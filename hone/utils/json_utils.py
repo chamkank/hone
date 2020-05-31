3e0ae87fd836c41f27b0bcce8a46c611d03d617f
CK <chamkank@users.noreply.github.com>
2020-05-30 19:15:26 -0700

    Add stdin/stdout support (closes #1)

--- hone/utils/json_utils.py
+++ hone/utils/json_utils.py
@@ -4,11 +4,15 @@ Simple methods for processing JSON files
 
 import os
 import json
+from sys import stdout
 
 '''
-Write given JSON to given file.
+Write given JSON to given file, or standard output if filepath is "-".
 '''
 
-def save_json(json_struct, json_filepath):
-    with open(json_filepath, 'w') as f:
-        json.dump(json_struct, f, indent=2, sort_keys=True)
+def output_json(json_struct, json_filepath):
+    if json_filepath and json_filepath == "-":
+        stdout.write(str(json_struct))
+    else:
+        with open(json_filepath, 'w') as f:
+            json.dump(json_struct, f, indent=2, sort_keys=True)
